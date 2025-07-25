bf3fd12c308235f9a2dc05981ae62ee4b13d2f2c
berth <berthortizweb@gmail.com>
2025-07-24 23:18:53 -0600

    update

--- src/components/SectionAbout.jsx
+++ src/components/SectionAbout.jsx
@@ -10,9 +10,7 @@ export default function SectionAbout() {
                 <h2 className="font-bold text-2xl lg:text-3xl">Un poco sobre mi</h2>
                 <p className=" text-[var(--color-text-p)] md:w-[55ch] lg:w-[75ch]"> <br />Me llamo Wilberth Ortiz (Berth), Soy desarrollador web 💻 con experiencia en WordPress, especializado en crear sitios funcionalesr para emprendedores y pequeñas empresas.
 
-                    <br></br><br></br>Actualmente, estoy ampliando mis habilidades como desarrollador front-end, enfocándome en JavaScript y React para ofrecer soluciones más personalizadas.
-
-                    Me encanta resolver problemas con código y seguir aprendiendo nuevas tecnologías que me permitan entregar resultados de calidad profesional. <br></br><br></br>Me encanta lo que hago <span>❤️</span> </p>
+                    <br></br><br></br>Actualmente, estoy ampliando mis habilidades como desarrollador front-end, enfocándome en JavaScript y React para ofrecer soluciones más personalizadas.<br></br>Me encanta encontrar soluciones mediante el código y seguir aprendiendo nuevas tecnologías que me permitan entregar resultados con calidad profesional.<br></br><br></br>Me encanta lo que hago <span>❤️</span> </p>
 
             </div>
             <div className="my-auto" >
